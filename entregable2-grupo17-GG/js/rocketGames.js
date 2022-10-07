b6bffdc748de91f50063400f5b27ce23141070a7
Magali Garrido <magalisuyaygarrido@gmail.com>
2022-10-07 17:26:24 -0300

    Se acomoda cards

--- entregable2-grupo17-GG/js/rocketGames.js
+++ entregable2-grupo17-GG/js/rocketGames.js
@@ -2,9 +2,11 @@
 
 let btnMenu = document.querySelector("#btn-sidebar-menu");
 let btnCloseMenu = document.querySelector("#btn-close-menu");
+let btnDialogInfo = document.querySelector("#i-info-card");
 
 btnMenu.addEventListener("click", openMenu);
 btnCloseMenu.addEventListener("click", closeMenu);
+btnDialogInfo.addEventListener("click", openDialog);
 
 function openMenu() {
     document.querySelector("#sidebar-menu").style.width = "400px";
@@ -14,4 +16,8 @@ function closeMenu() {
     document.querySelector("#sidebar-menu").style.width = "0";
     document.querySelector("contenido-menu").style.marginLeft = "0";
     document.querySelector("#btn-close-menu").style.display = "none";
+}
+
+function openDialog() {
+    document.querySelector("#dialogo-info-juego").style.width = "400px"
 }
