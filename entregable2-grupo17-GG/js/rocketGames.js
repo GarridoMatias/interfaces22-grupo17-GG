65e91b33610a909933c46696324535635495e8ab
Magali Garrido <magalisuyaygarrido@gmail.com>
2022-10-10 15:54:47 -0300

    Se acomodan estilos

--- entregable2-grupo17-GG/js/rocketGames.js
+++ entregable2-grupo17-GG/js/rocketGames.js
@@ -9,62 +9,19 @@ let slideControlNext = document.querySelector("#slide-control-prev");
 
 let carrusel = document.querySelector("#container-cards");
 
-// let btnDialogBlood = document.querySelector("#i-info-blood");
-// let btnDialogHarry = document.querySelector("#i-info-harry");
-// let btnDialogSackBoy = document.querySelector("#i-info-sackboy");
-// let btnDialogMario = document.querySelector("#i-info-mario");
-// let btnDialogPlants = document.querySelector("#i-info-plants");
-// let btnDialogGuys = document.querySelector("#i-info-guys");
-// let btnDialogGot = document.querySelector("#i-info-got");
-// let btnDialogGow = document.querySelector("#i-info-gow");
+btnMenu.addEventListener("click", openMenu);
+btnCloseMenu.addEventListener("click", closeMenu);
 
-// btnMenu.addEventListener("click", openMenu);
-// btnCloseMenu.addEventListener("click", closeMenu);
-// btnDialogInfo.addEventListener("click", openDialog);
-// btnCloseDialog.addEventListener("click", closeDialog);
-// iconCompartir.addEventListener("click", compartir);
-// slideControlNext.addEventListener("click", slideNext);
-
-// btnDialogBlood.addEventListener("click", function(e) {
-//     openDialog("BloodBorne", "comprado")
-// });
-
-// btnDialogHarry.addEventListener("click", function(e) {
-//     openDialog("Harry Potter y la Piedra Filosofal", "agregado")
-// });
-
-// btnDialogSackBoy.addEventListener("click", function(e) {
-//     openDialog("Sackboy: A Big Adventure", "Gratis")
-// });
-
-// btnDialogMario.addEventListener("click", function(e) {
-//     openDialog("Mario Kart", "Gratis")
-// });
-
-// btnDialogPlants.addEventListener("click", function(e) {
-//     openDialog("Plantas & Zombies 3", "Gratis")
-// });
-
-// btnDialogGuys.addEventListener("click", function(e) {
-//     openDialog("Stumble Guys", "Gratis")
-// });
-
-// btnDialogGot.addEventListener("click", function(e) {
-//     openDialog("A Game of Thrones", "Gratis")
-// });
-
-// btnDialogGow.addEventListener("click", function(e) {
-//     openDialog("God of War", 1500)
-// });
-
+iconCompartir.addEventListener("click", compartir);
+slideControlNext.addEventListener("click", slideNext);
 
 function openMenu() {
     document.querySelector("#sidebar-menu").style.width = "400px";
+    document.querySelector("#btn-close-menu").style.display = "block";
 }
 
 function closeMenu(e) {
-    e.preventDefault(); // agregado para que no recargue la pagina al cerrar popap
-
+    e.preventDefault();
     document.querySelector("#sidebar-menu").style.width = "0";
     document.querySelector("#contenido-menu").style.marginLeft = "0";
     document.querySelector("#btn-close-menu").style.display = "none";
@@ -76,13 +33,10 @@ function openDialog(juego) {
     document.querySelector("#id-img-dialogo").src = `${juego.img}`;
     document.querySelector("#id-precio-dialogo").innerHTML = juego.precio;
     document.querySelector("#id-descripcion-dialogo").innerHTML = juego.descripcion;
-
 }
 
 function closeDialog() {
-
     document.querySelector("#dialogo-info-juego").style.width = "0";
-
 }
 
 function compartir() {
@@ -96,7 +50,7 @@ function slideNext() {
 btnCloseDialog.addEventListener("click", closeDialog);
 
 let btnDialogV2 = document.querySelectorAll(".i-info-card");
-btnDialogV2.forEach(b => b.addEventListener("click", function() {
+btnDialogV2.forEach(b => b.addEventListener("click", function () {
     let juego = b.getAttribute("id").split("-")[3];
     openDialog(juegos[juego]);
 }));
