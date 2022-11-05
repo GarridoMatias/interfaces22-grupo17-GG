c932cd357bf0e95de5ac8608a76f09b0d774e5a1
GarridoMatias <mgarrido@alumnos.exa.unicen.edu.ar>
2022-11-05 18:34:07 -0300

    correccion tp2 menu responsivo

--- entregable3-grupo17-GG/js/menu.js
+++ entregable3-grupo17-GG/js/menu.js
@@ -7,13 +7,13 @@ btnMenu.addEventListener("click", openMenu);
 btnCloseMenu.addEventListener("click", closeMenu);
 
 function openMenu() {
-    document.querySelector("#sidebar-menu").style.width = "400px";
+    document.querySelector("#sidebar-menu").style.display = "block";
     document.querySelector("#btn-close-menu").style.display = "block";
 }
 
 function closeMenu(e) {
     e.preventDefault();
-    document.querySelector("#sidebar-menu").style.width = "0";
+    document.querySelector("#sidebar-menu").style.display = "none";
     document.querySelector("#contenido-menu").style.marginLeft = "0";
     document.querySelector("#btn-close-menu").style.display = "none";
 }
