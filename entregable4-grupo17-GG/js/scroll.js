9f18f10e53b7ef260d78c5aadb47a17a2c6ea0d4
Magali Garrido <magalisuyaygarrido@gmail.com>
2022-11-21 13:44:21 -0300

    Se continua seccion hero

--- entregable4-grupo17-GG/js/scroll.js
+++ entregable4-grupo17-GG/js/scroll.js
@@ -24,10 +24,16 @@ let header = document.querySelector(".container-header");
 let contenidoHeader = document.querySelectorAll(".c-header");
 let textoHero
 let fechaLanzamiento
+let introduccion
+let carrusel
+let historia
 
 if (document.querySelector(".container-hero-texto")) {
     textoHero = document.querySelector(".container-hero-texto");
     fechaLanzamiento = document.querySelector(".container-fecha-lanzamiento");
+    introduccion = document.querySelector(".container-introduccion-lanzamiento");
+    carrusel = document.querySelector(".carrusel");
+    historia = document.querySelector(".container-historia");
 }
 
 window.onscroll = function() {
@@ -35,21 +41,70 @@ window.onscroll = function() {
 
     if (textoHero) {
         difuminar();
-        viewFechaLanzamiento();
+        dimensionar();
+        viewFechaLanzamiento();  
+        trasladar();
     }
 }
 
-function difuminar() {
-    if (window.pageYOffset > 100) {
+function difuminar() {  
+    if (window.pageYOffset > 150) {
         textoHero.classList.add("difuminado");
-        fechaLanzamiento.classList.remove("difuminado");
     } else {
         textoHero.classList.remove("difuminado");
-        fechaLanzamiento.classList.add("difuminado");
     }
+
+    if (window.pageYOffset > 200) {
+      introduccion.style.opacity = 1;
+    } else {
+      introduccion.style.opacity = 0;
+      introduccion.classList.add("difuminado");
+    }
+
+    if (window.pageYOffset > 320) {
+      carrusel.style.opacity = 1;
+    } else {
+      carrusel.style.opacity = 0;
+      carrusel.classList.add("difuminado");
+    }
+
+    if (window.pageYOffset > 1000) {
+      historia.style.opacity = 1;
+    } else {
+      historia.style.opacity = 0;
+      historia.classList.add("difuminado");
+    }
+
 }
 
-function viewFechaLanzamiento() {}
+function dimensionar(){
+
+  if (window.pageYOffset > 300) {
+    carrusel.classList.add("dimensionar");
+  } else {
+    carrusel.classList.remove("dimensionar");
+  }
+
+  if (window.pageYOffset > 1000) {
+    historia.classList.add("dimensionar");
+  } else {
+    historia.classList.remove("dimensionar");
+  }
+}
+
+function viewFechaLanzamiento() {
+  if (window.pageYOffset > 350) {
+    fechaLanzamiento.style.opacity = 1;
+    let posFechaLanzamiento = 1200 - (scrollY);
+    fechaLanzamiento.style.left = `${posFechaLanzamiento}px`    
+  } else {
+    fechaLanzamiento.style.opacity = 0;
+  }
+
+  if (window.pageYOffset > 1050) {
+    fechaLanzamiento.style.left = `5%`
+  }
+}
 
 function stickyHeader() {
     if (window.pageYOffset > header.scrollTop) {
